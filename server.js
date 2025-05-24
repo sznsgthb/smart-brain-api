4d911f0d123d29355cc146178a0715e2c982a943
sznsgthb <suzannehoftijzer@gmail.com>
2025-05-24 14:57:23 +0200

    updating database

--- server.js
+++ server.js
@@ -11,11 +11,11 @@ import { handleImage, handleApiCall } from './controllers/image.js';
 const db = knex ({
     client: 'pg',
     connection: {
-      host: '127.0.0.1',
+      host: process.env.DATABASE_HOST,
       port: 5432,
-      user: 'suzannehoftijzer',
-      password: '',
-      database: 'smart-brain',
+      user: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PW,
+      database: process.env.DATABASE_DB,
     },
   });
 
